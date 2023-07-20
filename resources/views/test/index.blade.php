c5e582bb04365d1a706050ced36c9999641eb45c
RRRza <testaaazx@gmail.com>
2023-07-20 14:48:45 +0700

    week4

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<h1>Gallery of รัชชานนท์ สมพื้น 64122420014</h1>
+ 
+ant <a href = "/newgallery/ant"><img src = "{{$ant}}" alt ="antman" style ="width:200px; height:100px;"></a>
+bird <a href = "/newgallery/bird"> <img src = "{{$bird}}" alt ="falcon" style ="width:200px; height:100px;"></a>
+cat  <a href = "/newgallery/cat"> <img src = "{{$cat}}" alt ="blackpanter" style ="width:200px; height:100px;"></a>
+god <a href = "/newgallery/god"> <img src = "{{$god}}" alt ="thor" style ="width:200px; height:100px;"></a>
+spider <a href = "/newgallery/spider"> <img src = "{{$spider}}" alt ="" style ="width:200px; height:100px;"></a>
+     
+</body>
+</html>    
